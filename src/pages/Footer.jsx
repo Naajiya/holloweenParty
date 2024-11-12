5cec37922a7d65762e0f5a482bafab7bf8f1e0a9
NAJIYA KP <najiyabinthmajeed@gmail.com>
2024-11-12 07:45:30 -0800

    add footer as responsive

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -4,25 +4,25 @@ import pumKin from '../assets/pumpum.png'
 function Footer() {
     return (
         <>
-            <div className='container-fluid' style={{backgroundColor:'#4B0082'}}>
+            <div className='container-fluid' style={{ backgroundColor: '#4B0082' }}>
                 <div className='container d-flex justify-content-center align-items-center'>
                     <img src={pumKin} alt="" />
                 </div>
-                <hr style={{color:'white', border:'2px solid white'}}></hr>
-                <div className='d-flex justify-content-between align-items-center pe-4 text-light m-2'>
-                    <div style={{fontFamily:'cursive',color:'red'}}>
+                <hr style={{ color: 'white', border: '2px solid white' }}></hr>
+                <div className='row d-flex justify-content-between text-center align-items-center pe-4 text-light m-2'>
+                    <div className='col-sm-12 col-lg-3 m-2 ' style={{ fontFamily: 'cursive', color: 'red' }}>
                         <h2>holloween</h2>
-                        </div>
-                <div className='d-flex justify-content-center align-items-center pe-4 text-light'>
-                    <div className='m-3'><i class="fa-brands fa-facebook fa-lg"></i></div>
-                    <div className='m-3'><i class="fa-brands fa-youtube fa-lg"></i></div>
-                    <div className='m-3'><i class="fa-brands fa-instagram fa-lg"></i></div>
-                </div>
-                <div style={{fontFamily:'cursive'}}>
-                    <p>Enter Your Location</p>
-                    <p>(123)-567 897 670</p>
-                    
-                </div>
+                    </div>
+                    <div className='col-sm-12 col-lg-3 m-2 d-flex justify-content-center align-items-center pe-4 text-light'>
+                        <div className='m-3'><i class="fa-brands fa-facebook fa-lg"></i></div>
+                        <div className='m-3'><i class="fa-brands fa-youtube fa-lg"></i></div>
+                        <div className='m-3'><i class="fa-brands fa-instagram fa-lg"></i></div>
+                    </div>
+                    <div className='col-sm-12 col-lg-3 m-2' style={{ fontFamily: 'cursive' }}>
+                        <p>Enter Your Location</p>
+                        <p>(123)-567 897 670</p>
+
+                    </div>
                 </div>
                 s
             </div>
